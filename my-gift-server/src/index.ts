f84ba532c41acb94785c06847972a1707dda774b
nomaprof <nomaprof@gmail.com>
2024-04-03 07:59:39 +0100

    Initializing Prisma 6

--- my-gift-server/src/index.ts
+++ my-gift-server/src/index.ts
@@ -8,14 +8,28 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
-app.get("/api/gifts", async (req, res) => {
-  res.json({ message: "success!" });
-});
-
 app.get("/api/notes", async (req, res) => {
   const notes = await prisma.note.findMany();
   res.json(notes);
 });
+
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+  }
+
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Oops, something went wrong");
+  }
+});
+
 
 app.listen(5432, () => {
   console.log("server running on localhost:5432");
