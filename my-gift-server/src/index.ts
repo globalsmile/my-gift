4fa1c9748648f8ed8fd21407a39fabfc73af4359
nomaprof <nomaprof@gmail.com>
2024-04-03 08:02:08 +0100

    Initializing Prisma 8

--- my-gift-server/src/index.ts
+++ my-gift-server/src/index.ts
@@ -53,6 +53,24 @@ app.put("/api/notes/:id", async (req, res) => {
   }
 });
 
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (!id || isNaN(id)) {
+    return res.status(400).send("ID field required");
+  }
+
+  try {
+    await prisma.note.delete({
+      where: { id },
+    });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send("Oops, something went wrong");
+  }
+});
+
+
 app.listen(5432, () => {
   console.log("server running on localhost:5432");
 });
