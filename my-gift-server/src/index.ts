553116fe3b197a3cf5edfef0748e8247644e91b0
nomaprof <nomaprof@gmail.com>
2024-04-03 07:56:34 +0100

    Initializing Prisma 5

--- my-gift-server/src/index.ts
+++ my-gift-server/src/index.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/gifts", async (req, res) => {
   res.json({ message: "success!" });
+});
+
+app.get("/api/notes", async (req, res) => {
+  const notes = await prisma.note.findMany();
+  res.json(notes);
 });
 
 app.listen(5432, () => {
